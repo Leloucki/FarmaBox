85d1a21e02ae1326abbbd68aa1365dd8c2c7e7a2
Leloucki <feliperaniere.g2609@gmail.com>
2022-11-30 15:53:59 -0300

    CORREÇÃO

--- resources/views/costumer/layout/footer.blade.php
+++ resources/views/costumer/layout/footer.blade.php
@@ -144,11 +144,9 @@
                             <div class="item-content" style="margin-top:-10px; padding-bottom: 10px;">
                                 <div class="wrap-list-item social-network">
                                     <ul>
-                                        <li><a href="" class="link-to-item" title="twitter"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
-                                        <li><a href="" class="link-to-item" title="facebook"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
-                                        <li><a href="" class="link-to-item" title="pinterest"><i class="fa fa-pinterest" aria-hidden="true"></i></a></li>
-                                        <li><a href="" class="link-to-item" title="instagram"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
-                                        <li><a href="" class="link-to-item" title="vimeo"><i class="fa fa-vimeo" aria-hidden="true"></i></a></li>
+                                        <li><a href="https://twitter.com/universidadeumc" class="link-to-item" title="twitter" target="_blank"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+                                        <li><a href="https://www.facebook.com/universidadeumc/" class="link-to-item" title="facebook" target="_blank"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+                                        <li><a href="https://www.instagram.com/universidadeumc" class="link-to-item" title="instagram" target="_blank"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
                                     </ul>
                                 </div>
                             </div>
